59a2c2eb46da036fc6384d103bf560fa5744899e
dhk465 <daehee.kim465@gmail.com>
2025-06-12 13:07:36 +0200

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,171 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import Notification from './components/Notification';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [notification, setNotification] = useState(null);
+  const [newBlog, setNewBlog] = useState({
+    title: '',
+    author: '',
+    url: '',
+  });
+
+  const notifyWith = (message, isError = false) => {
+    setNotification({ message, isError });
+    setTimeout(() => {
+      setNotification({ message: null });
+    }, 5000);
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      notifyWith('wrong username or password', true);
+    }
+  };
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    );
+  };
+
+  const logoutForm = () => {
+    return (
+      <div>
+        {user.name} logged in{' '}
+        <button
+          onClick={() => {
+            window.localStorage.removeItem('loggedBlogappUser');
+            setUser(null);
+          }}
+        >
+          logout
+        </button>
+      </div>
+    );
+  };
+
+  // TODO: all proper input fields to add a blog
+  const blogForm = () => (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          title:
+          <input
+            value={newBlog.title}
+            name="title"
+            onChange={handleBlogChange}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            value={newBlog.author}
+            name="author"
+            onChange={handleBlogChange}
+          />
+        </div>
+        <div>
+          url:
+          <input value={newBlog.url} name="url" onChange={handleBlogChange} />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  );
+
+  const addBlog = async (event) => {
+    event.preventDefault();
+
+    try {
+      const createdBlog = await blogService.create(newBlog);
+      setBlogs(blogs.concat(createdBlog));
+      notifyWith(`a new blog ${createdBlog.title} by ${createdBlog.author} added`);
+      setNewBlog({
+        title: '',
+        author: '',
+        url: '',
+      });
+    } catch (exception) {
+      notifyWith('Error creating blog', true);
+    }
+  };
+
+  const handleBlogChange = (event) => {
+    const { name, value } = event.target;
+    setNewBlog({
+      ...newBlog,
+      [name]: value,
+    });
+  };
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
 
-export default App
+      <Notification message={notification?.message} isError={notification?.isError} />
+
+      {user === null && loginForm()}
+      {user !== null && logoutForm()}
+
+      {user !== null && blogForm()}
+
+      {user !== null && blogs.map((blog) => <Blog key={blog.id} blog={blog} />)}
+    </div>
+  );
+};
+
+export default App;
